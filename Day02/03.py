0ae8586aa84bcb30c94b4a977bdc12dbc55e7d90
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-22 19:47:28 +0530

    Input Function

---
+++ Day02/03.py
@@ -0,0 +1,10 @@
+# input() Function
+# The input() function is used to take input from the user.
+# Whatever the user types is stored in a variable.
+
+name = input("Enter your name: ")
+print(name)  # prints the value entered by the user
+
+# input() always takes input as a string (str) even if you type a number.
+age = input("Enter your age: ")
+print(age)
