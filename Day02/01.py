bbb47708d1ac9b55c2be7af3f0ac76e9bf9e8bf8
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-22 19:41:04 +0530

    Variables

---
+++ Day02/01.py
@@ -0,0 +1,15 @@
+# A variable is used to store data in Python.
+
+name = "Sahil"  # To store words/text, we use quotes. This is called a string.
+age = 20        # For numerical values, we write the number directly (no quotes).
+
+x = 5
+y = 10
+
+print(age)      # Output: 20 (because we printed the variable without quotes)
+
+print("x")      # Output: x (not 5) because quotes mean it is text, not a variable
+print(x)        # Output: 5 (because this prints the value stored in variable x)
+
+# What Is String?
+# A string (str) is a datatype used to store text (letters, words, sentences, symbols).
