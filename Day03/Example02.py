cb8a8222242f1fb05d66d0bcccd6d22fff00d7c3
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-23 22:10:38 +0530

    Input & Type Check

---
+++ Day03/Example02.py
@@ -0,0 +1,7 @@
+# Data Types - Input & Type Check
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Name:", name, "| Type:", type(name))
+print("Age:", age, "| Type:", type(age))   # age is string because input() gives str
