4e8767113880bcb3132e9cf79eed25f656ff26f8
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-23 22:02:53 +0530

    Data Type

---
+++ Day03/01.py
@@ -0,0 +1,28 @@
+# Day 03 - Data Types in Python
+# A data type tells Python what kind of value is stored in a variable.
+
+# int (Integer): Used to store whole numbers.
+age = 20
+
+# float(): Used to store decimal numbers.
+cgpa = 8.5
+
+# str (String): Used to store text (words/sentences).
+OP = "The One Piece Is REAL!!"
+
+# bool (Boolean): Used to store True or False.
+is_student = True
+
+# type() function: We use type() to check the data type of a value/variable.
+x = 10
+print(type(x))
+
+# Type Casting: means converting one data type into another.
+a = "10"
+print(int(a))     # converts string to int
+
+b = "8.5"
+print(float(b))   # converts string to float
+
+c = 100
+print(str(c))     # converts int to string
