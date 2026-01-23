82f7985b69a31231848735e86c2eeff10d8e0e99
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-23 22:09:41 +0530

    Basic Example

---
+++ Day03/Example01.py
@@ -0,0 +1,11 @@
+# Data Types - Basic Example
+
+name = "Sahil"       # string
+age = 21             # int
+cgpa = 9.0           # float
+is_student = True    # bool
+
+print(name, type(name))
+print(age, type(age))
+print(cgpa, type(cgpa))
+print(is_student, type(is_student))
