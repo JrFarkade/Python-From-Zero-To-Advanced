858619c1517184ba53ecdfb1a44af8f50dd0fd6e
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-23 22:11:50 +0530

    Type Casting

---
+++ Day03/Example03.py
@@ -0,0 +1,11 @@
+# Data Types - Type Casting Example
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+a = int(a)
+b = int(b)
+
+print("Sum =", a + b)
+print("Type of a:", type(a))
+print("Type of b:", type(b))
