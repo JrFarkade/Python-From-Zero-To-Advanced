96ae03c254d80ecaee89ea0da6def60f27ca1216
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-24 22:59:05 +0530

    Logical Operators

---
+++ Day04/Logical Operators.py
@@ -0,0 +1,17 @@
+# Logical Operators
+# Used to combine conditions.
+
+# Operator	Meaning
+# and	        Both conditions must be True
+# or	        At least one condition True
+# not	        Reverse the result
+
+# Example
+# Logical Operators
+
+age = 22
+has_id = True
+
+print(age > 18 and has_id)
+print(age < 18 or has_id)
+print(not has_id)
