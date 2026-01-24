577941dfd098fc6b9d8bffee0b3059376362358a
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-24 22:56:28 +0530

    Arithmetic Operators

---
+++ Day04/Arithmetic Operators.py
@@ -0,0 +1,23 @@
+# Arithmetic Operators
+# Used to perform mathematical operations.
+
+# Operator	Meaning
+# +	        Addition
+# -	        Subtraction
+# *	        Multiplication
+# /	        Division
+# %	        Modulus (remainder)
+# **	        Power
+# //	        Floor division
+
+# Example
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+print("Floor Division:", a // b)
