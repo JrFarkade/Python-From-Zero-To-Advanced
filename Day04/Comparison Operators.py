86a83f0d323eaf0fe87f8e3bb5965ac8cfe04013
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-24 22:58:25 +0530

    Comparison Operators

---
+++ Day04/Comparison Operators.py
@@ -0,0 +1,21 @@
+# Comparison Operators
+# Used to compare two values. Output is True or False.
+
+# Operator	Meaning
+# ==	        Equal
+# !=	        Not equal
+# >	          Greater than
+# <	          Less than
+# >=	        Greater or equal
+# <=	        Less or equal
+
+# Example
+x = 10
+y = 5
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
