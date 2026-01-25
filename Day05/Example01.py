769de9b5a6bef9d359475549e111449f8185cd01
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-25 22:16:51 +0530

    Example

---
+++ Day05/Example01.py
@@ -0,0 +1,13 @@
+# 1) Take name and print first letter
+name = input("Enter your name: ")
+print("First letter:", name[0])
+
+# 2) Print last 3 characters
+print("Last 3 chars:", name[-3:])
+
+# 3) Convert input text to uppercase
+text = input("Enter text: ")
+print(text.upper())
+
+# 4) Count length of string
+print("Length:", len(text))
