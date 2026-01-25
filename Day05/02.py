fa6c753287f4bb934ba08cf16a959f8940f8da2f
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-25 22:13:49 +0530

    Indexing & Slicing

---
+++ Day05/02.py
@@ -0,0 +1,18 @@
+# String Indexing
+# Each character in a string has a position (index).
+# Index starts from 0.
+
+# Example:
+word = "Python"
+print(word[0])   # P
+print(word[3])   # h
+
+
+# String Slicing
+# Used to get part of a string.
+# Syntax: string[start:end]
+
+# Example:
+word = "Python"
+print(word[0:3])   # Pyt
+print(word[2:5])   # tho
