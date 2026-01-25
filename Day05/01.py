1cff74812eeba6e88edf06f448470a6398030a07
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-25 22:11:04 +0530

    Strings

---
+++ Day05/01.py
@@ -0,0 +1,11 @@
+# Strings in Python
+
+# A string is used to store text (letters, words, sentences).
+
+# Strings are written inside quotes:
+# - " "
+# - ' '
+
+# Example:
+name = "Sahil"
+print(name)
