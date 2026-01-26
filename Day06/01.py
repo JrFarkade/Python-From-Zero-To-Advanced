9606e66ae0bcf6a89d9eadf1151b8eac35a46d18
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-26 20:46:31 +0530

    Conditions

---
+++ Day06/01.py
@@ -0,0 +1,14 @@
+# if / else / elif
+
+# What is Conditional Statement?
+# Conditional statements are used to make decisions in Python.
+
+# Python checks a condition:
+# If condition is True → code runs
+# If condition is False → code is skipped
+
+
+# if Statement
+# Used when only one condition is needed.
+if age >= 18:
+    print("You are eligible to vote")
