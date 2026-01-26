91bd90b66825238845a18eaa64cd1a69ee62f248
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-26 20:52:25 +0530

    Example02

---
+++ Day06/Example02.py
@@ -0,0 +1,8 @@
+# if ram score 90% in exam And Save 3000 Rs, He can go to trip
+x = int(input("Ram Score In Exam: "))
+y = int(input("saved Money: "))
+
+if x >= 90 and y >= 3000:
+    print("Ram Can Go TO Trip")
+else:
+    print("He Can Not Go To Trip")    
