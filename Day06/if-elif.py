c15845077c42c25d356303c206b369e1b78a16b7
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-26 20:49:44 +0530

    if-elif

---
+++ Day06/if-elif.py
@@ -0,0 +1,10 @@
+# elif Statement
+# Used when we have multiple conditions.
+
+marks = 75
+if marks >= 90:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+else:
+    print("Grade C")
