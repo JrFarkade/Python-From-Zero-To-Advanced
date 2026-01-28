6577775b1b63c3d4217c1bca6beaba1204a52249
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-28 22:27:54 +0530

    pass

---
+++ Day08/pass.py
@@ -0,0 +1,11 @@
+# pass is a placeholder.
+# We use pass when Python expects a block of code, but we don’t want to write anything yet.
+
+number = 10
+
+if number > 5:
+    pass
+else:
+    print("Small number")
+
+print("Done")
