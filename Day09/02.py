1fd0f83d2f63c3fa945adca088c4c9a3a7998938
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-29 22:39:26 +0530

    Parameter

---
+++ Day09/02.py
@@ -0,0 +1,12 @@
+# A parameter is a variable written inside a function definition.
+# It is used to receive values when the function is called.
+
+def greet(name):   # name is parameter
+    print("Hello", name)
+
+greet("Sahil")    # Sahil is argument
+
+# name → parameter (inside function)
+# "Sahil" → argument (value passed)
+
+## A parameter is a variable used in a function to receive input values.
