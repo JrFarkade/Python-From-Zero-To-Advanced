9e42494a822bf450ae49fd95f3966d22673e515a
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-29 22:45:07 +0530

    Example

---
+++ Day09/Example01.py
@@ -0,0 +1,10 @@
+# Write a function that takes user details (first name, last name, address, mobile number) and prints a greeting message.
+
+def greet_user(first_name, last_name, adress_place, mobile_no):
+    print(f"Hi My {first_name}{last_name} i lived in {adress_place} with my parents my mobile numbers is {mobile_no} ")
+    print("Nice to meet you all")
+
+
+print("Start")
+greet_user("Sahil", "Farkade", "Nagpur", "9100000000")
+print("End")
