8dad7f9bc81d647f133d27df1e302b72a1903fd5
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-29 22:36:40 +0530

    Functions

---
+++ Day09/01.py
@@ -0,0 +1,9 @@
+# Functions
+# A function is a block of code that runs only when it is called.
+# Functions help us reuse code.
+
+# Example
+def greet():
+    print("Hello Python")
+
+greet() # this code run the print function which is inside of greet() 
