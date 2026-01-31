5892217e5db57b6addd94538dd2403b92b1295e7
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-31 21:46:17 +0530

    Tuple

---
+++ Day11/Tuple.py
@@ -0,0 +1,8 @@
+# Tuple
+# A tuple is used to store multiple values.
+# Tuples are written using () and cannot be changed.
+# But once created, tuple values cannot be changed (tuple is immutable).
+
+# Example:
+numbers = (10, 20, 30)
+print(numbers)
