1dd724288c03dce7c6f38697de4388c28b0762ef
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-31 21:49:29 +0530

    Set.

---
+++ Day11/Set.py
@@ -0,0 +1,7 @@
+# A set is used to store unique values.
+# Sets are written using {}.
+# A set does not allow duplicate values.
+# You can add or remove items, but you cannot access items by index.
+
+colors = {"Red", "Blue", "Green"}
+print(colors)
